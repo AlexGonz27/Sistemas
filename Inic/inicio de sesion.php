5b11a4a311a1f3b14a1f43e191fa931c7d2b5111
wilmermor <wilmermor3n000@gmail.com>
2024-07-16 20:15:02 -0400

    Update inicio de sesion.php

--- Inic/inicio de sesion.php
+++ Inic/inicio de sesion.php
@@ -21,7 +21,7 @@
      
          // Verificar la conexión
          if ($conn->connect_error) {
-             die("La conexión falló: " . $conn->connect_error);
+             die("La conexión falló: " . $conn->connect_error);//pendejo
          }
          return $conn;
       }
