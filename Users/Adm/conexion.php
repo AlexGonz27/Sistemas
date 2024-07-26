976fe5c150eed9461a83d68dc8e7076cb0296348
AlexGonz27 <alexagustingonzalez@gmail.com>
2024-07-26 15:16:30 -0400

    v0.4143 Conexion corrección

--- Users/Adm/conexion.php
+++ Users/Adm/conexion.php
@@ -4,9 +4,9 @@
         $databaseName = "b03oim8xwvf4jpuq5buf"; // Cambia esto al nombre de tu base de datos
         $username = "uqcyvv3hrdg9nufd"; // Cambia esto al nombre de usuario
         $password = "Rbi6QbmCFiZViAS8dcvY"; // Cambia esto a la contraseña
-        $port = "";
+        $port = "3306";
            // Crear conexión
-        $conn = new mysqli($serverName,$username, $password, $databaseName);
+        $conn = new mysqli($serverName, $username, $password, $databaseName, $port);
            // Verificar la conexión
         if ($conn->connect_error) {
            die("La conexión falló: " . $conn->connect_error);
