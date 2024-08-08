5b6fad16d72c2aca72015cdac30bd5eec480cfbf
wilmermor <wilmermor3n000@gmail.com>
2024-08-07 22:42:01 -0400

    Update categorias.php

--- backend/Adm/categorias/categorias.php
+++ backend/Adm/categorias/categorias.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //LOGEADO??
 session_start();
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
@@ -15,6 +15,10 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categorias</title>
     <link rel="stylesheet" href="./estilos.css">
+    <!-- Bootstrap 5 CDN Links-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Alertas -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
@@ -156,106 +160,138 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                     <img src="assets/imgs/customer01.jpg" alt="">
                 </div>
             </div>
-        <div> 
-    </div>
-    <div class="dt-serv">
-        <div class="serviciosTable">
-            <div class="cartaHeader">
-                <h2>Categorias</h2>
+            <div>
             </div>
-            <div class="conte-btns">
-                <div>
-                    <div class="btn-agregar" onclick="document.getElementById('ventagregar').style.display = 'block'">Agregar</div>
-                </div>
-                <div>
-                    <button id="consulta-btn" onclick="tareaCompletada()" style="display: none;">Hacer Consulta</button>
-                    <input id="buscador_tabla" type="text" placeholder="Buscar">
-                </div>
-            </div>
+            <div class="dt-serv">
+                <div class="serviciosTable">
+                    <div class="cartaHeader">
+                        <h2>Categorias</h2>
+                    </div>
+                    <div class="conte-btns">
+                        <div>
+                            <div class="btn-agregar" data-bs-toggle="modal" data-bs-target="#ventagregar">
+                                Agregar
+                            </div>
+                        </div>
+                        <div>
+                            <button id="consulta-btn" onclick="tareaCompletada()" style="display: none;">Hacer Consulta</button>
+                            <input id="buscador_tabla" type="text" placeholder="Buscar">
+                        </div>
+                    </div>
 
-            <table id="Tabla_Datos">
-                <thead>
-                    <tr>
-                        <td>Nombre</td>
-                        <td>Descripción</td>
-                        <td>Capacidad</td>
-                        <td>Precio</td>
-                        <td>Acciones</td>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                    include '../conexion.php';
-                    $conn = conectarDB();
-                    $sql = "SELECT * FROM tbl_categorias;";
-                    $resultado = mysqli_query($conn, $sql);
-                    while ($fila = mysqli_fetch_assoc($resultado)) {
-                        echo "<tr>
+                    <table id="Tabla_Datos">
+                        <thead>
+                            <tr>
+                                <td>Nombre</td>
+                                <td>Descripción</td>
+                                <td>Capacidad</td>
+                                <td>Precio</td>
+                                <td>Acciones</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include '../conexion.php';
+                            $conn = conectarDB();
+                            $sql = "SELECT * FROM tbl_categorias;";
+                            $resultado = mysqli_query($conn, $sql);
+                            while ($fila = mysqli_fetch_assoc($resultado)) {
+                                echo "<tr>
                                 <td>" . $fila['Nombre'] . "</td>
                                 <td>" . $fila['Descripción'] . "</td>
                                 <td>" . $fila['Capacidad'] . "</td>
                                 <td>" . $fila['Precio'] . "$</td>
                                 <td>
-                                    <span class='btns btn-modificar' onclick='ConfgVentModifiCat(".json_encode($fila).")'>Modificar</span>
-                                    <span class='btns btn-eliminar' onclick='ConfgVentElimCat(".$fila['ID_Categoria'].");'>Eliminar</span>
+                                    <span class='btns btn-modificar' onclick='ConfgVentModifiCat(" . json_encode($fila) . ")'>Modificar</span>
+                                    <span class='btns btn-eliminar' onclick='ConfgVentElimCat(" . $fila['ID_Categoria'] . ");'>Eliminar</span>
                                 </td>
                             </tr>";
-                    }
-                ?>
-                </tbody>
-            </table>
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div></div>    
-    
-    <div id="ventagregar" class="ventana">
-        <div class="conte-vent">
-            <ion-icon name="close-circle-outline" class="btns btn-cerrar" onclick="document.getElementById('ventagregar').style.display = 'none';"></ion-icon>
-            <form class="forma" id="form-agregar" action="" method="post">
-                <input type="hidden" name="agregar">
-                <input id="text-nombre" name="nombre" type="text" placeholder="Nombre">
-                <input id="text-desc" name="desc" type="text" placeholder="Descripcion">
-                <input id="text-cap" name="cap" type="int" placeholder="Capacidad">
-                <input id="text-cost" name="cost" type="text" placeholder="Precio">
-                <button class="btns btn-agregar" type="submit">Agregar</button>
-            </form>
-        </div>
-    </div>
 
-    <div id="ventmodifi" class="ventana">
-        <div class="conte-vent">
-            <ion-icon name="close-circle-outline" class="btns btn-cerrar" onclick="document.getElementById('ventmodifi').style.display = 'none';"></ion-icon>
-            <form class="forma" id="form-modificar"action="" method="post" name="modificar">
-                <input type="hidden" name="modificar">
-                <input id="ID_Cat" type="hidden" name="ID_Cat">
-                <input id="text-nombreCat" name="nombre" type="text" placeholder="Nombre">
-                <input id="text-descCat" name="desc" type="text" placeholder="Descripcion">
-                <input id="text-capCat" name="cap" type="int" placeholder="Capacidad">
-                <input id="text-costCat" name="cost" type="text" placeholder="Precio">
-                <button class="btns btn-modificar"  type="submit" class="forma btn-modificar">modificar</button>
-            </form>
-        </div>
-    </div>
+        <div id="ventagregar" class="modal fade" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">Agregar Categoría</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form id="form-agregar" action="" method="post">
+                        <input type="hidden" name="agregar">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="text-nombre" class="form-label">Nombre:</label>
+                                <input id="text-nombre" name="nombre" type="text" class="form-control" placeholder="Nombre" required>
+                            </div>
 
-    <div id="venteliminar" class="ventana">
-        <div class="conte-vent">
-            <ion-icon name="close-circle-outline" class="btns btn-cerrar" onclick="document.getElementById('venteliminar').style.display = 'none';"></ion-icon>
-            <form class="forma" id="form-agregar" action="" method="post" name="agregar">
-                <input type="hidden" name="eliminar">
-                <input id="ID_CatElim" type="hidden" name="ID_Cat">
-                <p>Seguro que desea eliminar esta fila?</p>
-                <button type="submit">eliminar</button>
-            </form>
-        </div>
-    </div>
-    <!-- =========== Scripts =========  -->
-    <script src="main.js"></script>
-    <script src="/Sistemas/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+                            <div class="mb-3">
+                                <label for="text-desc" class="form-label">Descripción:</label>
+                                <input id="text-desc" name="desc" type="text" class="form-control" placeholder="Descripción" required>
+                            </div>
 
-    <!-- ====== ionicons ======= -->
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                            <div class="mb-3">
+                                <label for="text-cap" class="form-label">Capacidad:</label>
+                                <input id="text-cap" name="cap" type="number" class="form-control" placeholder="Capacidad" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="text-cost" class="form-label">Precio:</label>
+                                <input id="text-cost" name="cost" type="text" class="form-control" placeholder="Precio" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button class="btn btn-primary" type="submit">Agregar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div id="ventmodifi" class="ventana">
+            <div class="conte-vent">
+                <ion-icon name="close-circle-outline" class="btns btn-cerrar" onclick="document.getElementById('ventmodifi').style.display = 'none';"></ion-icon>
+                <form class="forma" id="form-modificar" action="" method="post" name="modificar">
+                    <input type="hidden" name="modificar">
+                    <input id="ID_Cat" type="hidden" name="ID_Cat">
+                    <input id="text-nombreCat" name="nombre" type="text" placeholder="Nombre">
+                    <input id="text-descCat" name="desc" type="text" placeholder="Descripcion">
+                    <input id="text-capCat" name="cap" type="int" placeholder="Capacidad">
+                    <input id="text-costCat" name="cost" type="text" placeholder="Precio">
+                    <button class="btns btn-modificar" type="submit" class="forma btn-modificar">modificar</button>
+                </form>
+            </div>
+        </div>
+
+        <div id="venteliminar" class="ventana">
+            <div class="conte-vent">
+                <ion-icon name="close-circle-outline" class="btns btn-cerrar" onclick="document.getElementById('venteliminar').style.display = 'none';"></ion-icon>
+                <form class="forma" id="form-agregar" action="" method="post" name="agregar">
+                    <input type="hidden" name="eliminar">
+                    <input id="ID_CatElim" type="hidden" name="ID_Cat">
+                    <p>Seguro que desea eliminar esta fila?</p>
+                    <button type="submit">eliminar</button>
+                </form>
+            </div>
+        </div>
+        <!--Bootstrap 5 JS CDN Links -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+        <!-- =========== Scripts =========  -->
+        <script src="main.js"></script>
+        <script src="/Sistemas/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js"></script>
+
+        <!-- ====== ionicons ======= -->
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
